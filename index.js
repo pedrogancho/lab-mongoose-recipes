c4712c83d81238a92603401478d8e7964148f757
Pedro Gancho <pedro.gancho@inlifeportugal.com>
2021-11-12 17:55:10 +0000

    Completed lab

--- index.js
+++ index.js
@@ -20,8 +20,41 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: "Ramen",
+      level: "Amateur Chef",
+      ingredients: ["Rice", "Meat", "Eggs"],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: "30"
+      creator: "Uros"
+      created: "15/06/2020"
+    }
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  const Recipe = mongoose.('Recipe', recipeSchema) {
+    console.log ("recipeSchema.title")}
+
+
+  Recipe.deleteMany() 
+    .then(() => Recipe.insertMany(data)) 
+
+    .then(recipes => {
+      console.log("recipes added!")
+      return Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+    })
+    .then(recipe => {
+      console.log("recipe updated")
+      return Recipe.deleteOne({title: "Carrot Cake"}) 
+    })
+    .then(info => {
+      console.log("recipe deleted", info);
+    }) 
+    .then(() => mongoose.disconnect())
+    .catch(err => {console.log(err)})
+  
+  
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,8 +3,13 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-});
-
-const Recipe = mongoose.model('Recipe', recipeSchema);
-
-module.exports = Recipe;
+  title: {type: String, required: true, unique: true},
+  level: {type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+  ingredients: {type: [String]},
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enum: ["Breakfast", "main_course", "Soup", "Drink", "Dessert", "Other"]},
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type: Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
+})
